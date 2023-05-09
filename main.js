003aacbe7f8821833610098269da58e8a37588d3
queenly9 <128353487+queenly9@users.noreply.github.com>
2023-05-09 18:46:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -36,13 +36,27 @@ function blink() {
 setInterval(blink, 500);
 
 let hiddenElement = document.querySelector(".hidden");
+let hiddenElement1 = document.querySelector(".hidden1");
 
 function handleScroll() {
   const scrollPosition = window.scrollY;
 
   if (scrollPosition > 200) {
     hiddenElement.classList.add("visible");
+  } else {
+    hiddenElement.classList.remove("visible"); // 스크롤 위치가 200 이하인 경우 클래스 제거
+  }
+}
+
+function handleScroll1() {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 1000) {
+    hiddenElement1.classList.add("visible");
+  } else {
+    hiddenElement1.classList.remove("visible"); // 스크롤 위치가 200 이하인 경우 클래스 제거
   }
 }
 
 window.addEventListener("scroll", handleScroll);
+window.addEventListener("scroll", handleScroll1);
