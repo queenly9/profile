e69f9edbe4127ee8fbe34467ee0f5e49a5e4a5fc
queenly9 <128353487+queenly9@users.noreply.github.com>
2023-05-10 17:43:38 +0900

    최종본.js

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function handleScroll2() {
   if (scrollPosition > 400) {
     hiddenElement2.classList.add("visible");
   } else {
-    hiddenElement2.classList.remove("visible"); // 스크롤 위치가 200 이하인 경우 클래스 제거
+    hiddenElement2.classList.remove("visible");
   }
 }
 
@@ -55,7 +55,7 @@ function handleScroll() {
   if (scrollPosition > 1000) {
     hiddenElement.classList.add("visible");
   } else {
-    hiddenElement.classList.remove("visible"); // 스크롤 위치가 200 이하인 경우 클래스 제거
+    hiddenElement.classList.remove("visible");
   }
 }
 
@@ -65,10 +65,33 @@ function handleScroll1() {
   if (scrollPosition > 2000) {
     hiddenElement1.classList.add("visible");
   } else {
-    hiddenElement1.classList.remove("visible"); // 스크롤 위치가 200 이하인 경우 클래스 제거
+    hiddenElement1.classList.remove("visible");
   }
 }
 
 window.addEventListener("scroll", handleScroll);
 window.addEventListener("scroll", handleScroll1);
 window.addEventListener("scroll", handleScroll2);
+
+var Target = document.getElementById("clock");
+function clock() {
+  var time = new Date();
+
+  var year = time.getFullYear();
+  var month = time.getMonth();
+  var date = time.getDate();
+  var day = time.getDay();
+  var week = ["일", "월", "화", "수", "목", "금", "토"];
+
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+
+  Target.innerText =
+    `${year}년 ${month + 1}월 ${date}일 ${week[day]}요일 ` +
+    `${hours < 10 ? `0${hours}` : hours}:${
+      minutes < 10 ? `0${minutes}` : minutes
+    }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+clock();
+setInterval(clock, 1000);
